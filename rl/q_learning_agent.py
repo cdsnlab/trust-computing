1c7d0c6cdce1d3b4e43799c52097c70bcc19ff7c
Spencer90 <spencerjang90@gmail.com>
2020-08-26 14:33:48 +0900

    moved 'dynamic trust threshold' as the STATE

---
+++ rl/q_learning_agent.py
@@ -0,0 +1,94 @@
+#* general
+import numpy as np
+import pandas as pd
+import os
+from collections import defaultdict
+import random
+
+import matplotlib.pyplot as plt
+
+#* for NN models
+# from keras.models import Sequential
+# from keras.layers import Dense, Activation, Flatten
+# from keras.optimizers import Adam
+#* for RL 
+from trustEnv import trustEnv
+# import gym
+# from gym import spaces
+# from gym.utils import seeding
+# from rl.agents.dqn import DQNAgent
+# from rl.policy import EpsGreedyQPolicy
+# from rl.memory import SequentialMemory
+    
+class QLearningAgent:
+    def __init__ (self, actions):
+        self.actions = actions
+        self.learning_rate = 0.01
+        self.discount_factor = 0.9
+        self.epsilon = 0.1
+        #self.trust_threshold = 70
+        self.q_table = defaultdict(lambda:[0, 0, 0])
+
+
+
+    def learn (self, state, action, reward, next_state):
+        current_q = self.q_table[state][action]
+        # using Bellman Optimality Equation to update q function
+        new_q = reward + self.discount_factor * max(self.q_table[next_state])
+        self.q_table[state][action] += self.learning_rate * (new_q - current_q)
+
+    def get_action(self, state):
+        if np.random.rand() < self.epsilon:
+            # take random action
+            action = np.random.choice(self.actions)
+        else:
+            # take action according to the q function table
+            state_action = self.q_table[state]
+            action = self.arg_max(state_action)
+        return action
+
+    @staticmethod
+    def arg_max(state_action):
+        max_index_list = []
+        max_value = state_action[0]
+        for index, value in enumerate(state_action):
+            if value > max_value:
+                max_index_list.clear()
+                max_value = value
+                max_index_list.append(index)
+            elif value == max_value:
+                max_index_list.append(index)
+        return random.choice(max_index_list)
+
+
+if __name__ == "__main__":
+    env = trustEnv()
+    agent = QLearningAgent(actions=list(range(env.n_actions)))
+   
+    state = env.state
+    while True:
+        
+        print("[INFO] ", state)
+        # take action and proceed one step in the environment
+        action = agent.get_action(str(state))
+        
+        reward, next_state = env.step(action)
+
+        # with sample <s,a,r,s'>, agent learns new q function
+        agent.learn(str(state), action, reward, str(next_state))
+
+        # if episode ends, then break
+        if env.next_car_index == 99999:
+            break
+        state = next_state
+        #print(agent.q_table)
+
+
+
+#policy = EpsGreedyQPolicy()
+#memory = SequentialMemory(limit=50000, window_length=1)
+
+#dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10, target_model_update=1e-2, policy=policy)
+#dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+#dqn.fit(env, nb_steps=5000, visualize=False, verbose=2)
--- rl/trustEnv.py
+++ rl/trustEnv.py
@@ -7,6 +7,10 @@ import os
 import gym
 from gym import spaces
 from gym.utils import seeding
+from collections import defaultdict
+#* draw now
+import matplotlib.pyplot as plt
+
 
 
 N_DISCRETE_ACTIONS = 2
@@ -16,19 +20,81 @@ class trustEnv(gym.Env):
         the goal of this program is train our system to set dynamic trust threshold 
         
     """
-    def __init__(self, data):
+    def __init__(self):
         super(trustEnv, self).__init__()
-        self.df = data
-        #* creates two actions: increase t_threshold OR decrease t_threshold
-        self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
-        #* creates all possible spaces 
-        self.get_space_matrix(self.df)
-        
+        self.load_dataset('../sampledata/additional_features_with_predictions.csv') # load dataset
+        #* creates 3 actions: increase t_threshold OR decrease t_threshold
+        self.action_space = ["u", "d", "s"] #* 0, 1, 2
+        self.n_actions = len(self.action_space)
+        self.cases = defaultdict(lambda:[1, 1, 1]) #* total number of cases, correct cases, wrong cases
+        self.state = 80
+        self.next_car_index = 1
 
-    def step(self, a):
+    def load_dataset(self, filename):
+        #* read dataset
+        print("[INFO] Reading file...")
+        #data = pd.read_csv('../sampledata/data_6_.txt', sep='\t', header=0)
+        self.data = pd.read_csv(filename, header=0)
+        print("[INFO] File loaded")
+       
+
+    def step(self, action):
         #* inputs an action to current state & gets a reward to this action
+        DELAY = 10
+        delta = 5
+        #! 당연하게도 0에 수렴하겠지... 왜냐하면 올려도 threshold보다 크면 그만이고, 줄여도 threshold보다 크면 그만이니깐...
+        #! 어케 따라다니게 만들지?
 
-        return observed_state, reward
+        e_trust_val = int(self.data['I_trust_val'][self.next_car_index]*100)
+        self.cases["p"][0] +=1
+        #* adjust tthreshold
+        if action == 0: #up
+            if self.state < 100:
+                self.state+=delta
+        elif action == 1: #down
+            if self.state > 0:
+                self.state-=delta
+        else:
+            self.state-=0
+        print("[INFO] action: {}, tthreshold: {}, estimated trust value: {}, ".format(action, self.state, e_trust_val))
+
+        #* check if trust value is higher or lower than tthreshold.
+
+        if e_trust_val > self.state: 
+            self.cases["p"][1] +=1
+            result=1
+        else:
+            self.cases["p"][2] +=1
+            result=0
+        #! 이전 trust값과 이전 feedback으로 계산해야지 밥팅아 
+        print(self.next_car_index, DELAY)
+        if self.next_car_index > DELAY:
+            self.cases['g'][0]+=1
+            feedback = int(self.data['actual_status'][self.next_car_index-DELAY])
+            if result == feedback:
+                reward = 1
+                self.cases["g"][1]+=1
+            else: #* 구라친 경우
+                print("[DEBUG] FFFFFFFFFFFFFFFFFFFFFFFF")
+                reward = -100
+                self.cases["g"][2]+=1
+        else:
+            reward = 0
+
+        
+        print(self.cases['p'])
+        print("Perceived Accuracy: {}".format(self.cases["p"][1]/self.cases["p"][0]))
+        print(self.cases['g'])
+        print("GT Accuracy: {}".format(self.cases["g"][1]/self.cases["g"][0]))
+
+        self.next_car_index+=1
+
+        if self.next_car_index == 99999:
+            self.drawgraph()
+        return reward, self.state
+    
+    def drawgraph(self):
+        
 
     def reset(self):
         #* theres no need for our program to reset.. i think
@@ -38,15 +104,15 @@ class trustEnv(gym.Env):
         # Render the environment to the screen. Don't think we need this though
         print("rendering")
         
-    def get_space_matrix(self, df):
-        #* returns all available spaces
-        colname = list(df.columns)
-        print(colname)
-        #! requires min max of each category 
-        #! typically from 0 ~ 100 should be enough.
-        mmax = list(df.max(axis=0))
-        print(mmax)
-        mmin = list(df.min(axis=0))
-        print(mmin)
-        #self.observation_space = spaces.Dict({""})
+    # def get_space_matrix(self, df):
+    #     #* returns all available spaces
+    #     colname = list(df.columns)
+    #     print(colname)
+    #     #! requires min max of each category 
+    #     #! typically from 0 ~ 100 should be enough.
+    #     mmax = list(df.max(axis=0))
+    #     print(mmax)
+    #     mmin = list(df.min(axis=0))
+    #     print(mmin)
+    #     #self.observation_space = spaces.Dict({""})
         
