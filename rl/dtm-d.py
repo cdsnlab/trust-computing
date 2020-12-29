f014893dc89dbafed740cdb0553f8fc90be0d240
Spencer90 <spencerjang90@gmail.com>
2020-12-29 17:38:54 +0900

    fixed npv, ppv logic

--- rl/dtm-d.py
+++ rl/dtm-d.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 from pymongo import MongoClient
 from itertools import product, starmap
 
-result_values = defaultdict(lambda:[0,0,0]) #* precision, accuracy, recall
+result_values = defaultdict(lambda:[0,0,0, 0]) #* precision, accuracy, recall, f1
 cases = defaultdict(lambda:[0, 0, 0, 0]) #* TP, FP, FN, TN
 final = defaultdict(list)
 def named_product(**items):
@@ -35,9 +35,9 @@ def evaluate(threshold, interval, data, nci):
         # print(nci-i, decision[nci-i], int(data['status'][nci-i]))
 
         #* verify if its validity
-        if decision[nci-i] == 1 and int(data['status'][nci-i]==1):
+        if decision[nci-i] == 1 and int(data['status'][nci-i]==1): #* TP: malicious!
             cases['gt'][0]+=1
-        elif decision[nci-i] == 1 and int(data['status'][nci-i]==0):
+        elif decision[nci-i] == 1 and int(data['status'][nci-i]==0): #* FP: thought malicious but not!
             cases['gt'][1]+=1
         elif decision[nci-i] == 0 and int(data['status'][nci-i]==1):
             cases['gt'][2]+=1
@@ -60,18 +60,28 @@ def evaluate(threshold, interval, data, nci):
         result_values[nci][2]=100
     else:
         result_values[nci][2] = (cases["gt"][0])/(cases["gt"][0] + cases["gt"][2]) *100
+    #* f1 score
+    if result_values[nci][2]+result_values[nci][0] == 0:
+        result_values[nci][3]=0
+    else:
+        result_values[nci][3]= (2*result_values[nci][2]*result_values[nci][0]) / (result_values[nci][2] + result_values[nci][0]) 
+    # print(nci, result_values[nci], threshold) 
     # print(nci, result_values[nci]) 
     # print(nci, cases)
-    final['acc'].append(result_values[nci][0])
-    final['pre'].append(result_values[nci][1])
+    final['acc'].append(result_values[nci][1])
+    final['pre'].append(result_values[nci][0])
     final['rec'].append(result_values[nci][2])
+    final['f1'].append(result_values[nci][3])
+    final['dtt'].append(threshold)
+
+
     # return result_values
 
 connection = connect()
-for output in named_product(v_s = [11000], v_mvp=[0.2], v_mbp=[0.5], v_oap=[0.2, 0.4], v_interval=[100]): 
+for output in named_product(v_s = [20000], v_mvp=[0.2], v_mbp=[0.5], v_oap=[0.2], v_interval=[100]): 
     threshold=50
 
-    filename = "rl_df_"+str(output.v_mbp)+"mbp"+str(output.v_oap)+"oap"+str(output.v_mvp)+"mvp.csv"
+    filename = "cares_df_0_"+str(output.v_mbp)+"mbp"+str(output.v_oap)+"oap"+str(output.v_mvp)+"mvp.csv"
     data = pd.read_csv('../sampledata/'+filename, header=0)
     INTERVAL = output.v_interval
     next_car_index=1
@@ -81,22 +91,27 @@ for output in named_product(v_s = [11000], v_mvp=[0.2], v_mbp=[0.5], v_oap=[0.2,
         if next_car_index % INTERVAL ==0:
             evaluate(threshold, INTERVAL, data, next_car_index)
 
-        # if next_car_index % 200 == 0:
-
-            PPV = cases['gt'][0] / (cases['gt'][0] + cases['gt'][1])
+            if (cases['gt'][0] + cases['gt'][1]) == 0:
+                PPV = 0
+            else:
+                PPV = cases['gt'][0] / (cases['gt'][0] + cases['gt'][1])
             if PPV < 0.95:
                 threshold-=5
-                if threshold <0:
+                if threshold<0:
                     threshold=0
-            NPV = cases['gt'][3] / (cases['gt'][3] + cases['gt'][2])
+            if (cases['gt'][3] + cases['gt'][2]) == 0:
+                NPV = 0
+            else:
+                NPV = cases['gt'][3] / (cases['gt'][3] + cases['gt'][2])
             if NPV < 0.95:
                 threshold+=5
-                if threshold>100:
+                if threshold >100:
                     threshold=100
-            
-            print(next_car_index,  threshold, PPV, NPV)
+
+
+            print(next_car_index,  threshold, PPV, NPV, final['acc'][-1])
         if next_car_index == (output.v_s):
-            row = {"id": str(output), 'v_mvp': output.v_mvp, 'v_mbp': output.v_mbp, 'v_oap': output.v_oap, 'v_interval':output.v_interval, "v_s": output.v_s, "accuracy": final['acc'], 'precision': final['pre'], 'recall': final['rec']}
+            row = {"id": str(output), 'v_mvp': output.v_mvp, 'v_mbp': output.v_mbp, 'v_oap': output.v_oap, 'v_interval':output.v_interval, "v_s": output.v_s, "accuracy": final['acc'], 'precision': final['pre'], 'recall': final['rec'], 'dtt': final['dtt']}
             # connection.insert_one(row)
             print(final['acc'][-1])
             break
--- rl/dtm.py
+++ rl/dtm.py
@@ -1,5 +1,4 @@
-#* general
-import time
+#! done here!
 import pandas as pd
 from collections import namedtuple
 
@@ -7,7 +6,7 @@ from collections import defaultdict
 from pymongo import MongoClient
 from itertools import product, starmap
 
-result_values = defaultdict(lambda:[0,0,0]) #* precision, accuracy, recall
+result_values = defaultdict(lambda:[0,0,0,0]) #* precision, accuracy, recall, f1
 cases = defaultdict(lambda:[0, 0, 0, 0]) #* TP, FP, FN, TN
 final = defaultdict(list)
 def named_product(**items):
@@ -44,46 +43,55 @@ def evaluate(threshold, interval, data, nci):
         else:
             cases['gt'][3]+=1
     # print(cases)
-    #* calucalte precision, accuracy, recall
+    #* calucalte precision, accuracy, recall, f1
     #* precision
     if (cases["gt"][0] + cases["gt"][1]) == 0:
-        result_values[nci][0]=100
+        result_values[nci][0]=0
     else:
         result_values[nci][0] = (cases["gt"][0])/(cases["gt"][0] + cases["gt"][1]) *100 
     #* accuracy
     if (cases["gt"][0] + cases["gt"][1] +  cases["gt"][2] + cases["gt"][3]) ==0:
-        result_values[nci][1] =100
+        result_values[nci][1] =0
     else:
         result_values[nci][1] = (cases["gt"][0] + cases["gt"][3])/(cases["gt"][0] + cases["gt"][1] + cases["gt"][2] + cases["gt"][3]) *100 
     #* recall
     if (cases["gt"][0] + cases["gt"][2]) == 0:
-        result_values[nci][2]=100
+        result_values[nci][2]=0
     else:
         result_values[nci][2] = (cases["gt"][0])/(cases["gt"][0] + cases["gt"][2]) *100
-    # print(nci, result_values[nci]) 
+    #* f1 score
+    if result_values[nci][2]+result_values[nci][0] == 0:
+        result_values[nci][3]=0
+    else:
+        result_values[nci][3]= (2*result_values[nci][2]*result_values[nci][0]) / (result_values[nci][2] + result_values[nci][0]) 
+    print(nci, result_values[nci]) 
     # print(nci, cases)
-    final['acc'].append(result_values[nci][0])
-    final['pre'].append(result_values[nci][1])
+    final['pre'].append(result_values[nci][0])
+    final['acc'].append(result_values[nci][1])
     final['rec'].append(result_values[nci][2])
+    final['f1'].append(result_values[nci][3])
+    final['dtt'].append(threshold)
+
     # return result_values
 
 connection = connect()
-for output in named_product(v_s = [11000], v_mvp=[0.2], v_mbp=[0.5], v_oap=[0.2, 0.4], v_interval=[100]): 
+for output in named_product(v_s = [59999], v_mvp=[0.2], v_mbp=[0.5], v_oap=[0.2], v_interval=[100]): 
     threshold=50
 
-    filename = "rl_df_"+str(output.v_mbp)+"mbp"+str(output.v_oap)+"oap"+str(output.v_mvp)+"mvp.csv"
+    filename = "cares_df_0_"+str(output.v_mbp)+"mbp"+str(output.v_oap)+"oap"+str(output.v_mvp)+"mvp.csv"
     data = pd.read_csv('../sampledata/'+filename, header=0)
     INTERVAL = output.v_interval
     next_car_index=1
-    time.sleep(0.1)
+    # time.sleep(0.1)
     while True:
 
         if next_car_index % INTERVAL ==0:
             evaluate(threshold, INTERVAL, data, next_car_index)
         if next_car_index == (output.v_s):
-            row = {"id": str(output), 'v_mvp': output.v_mvp, 'v_mbp': output.v_mbp, 'v_oap': output.v_oap, 'v_interval':output.v_interval, "v_s": output.v_s, "accuracy": final['acc'], 'precision': final['pre'], 'recall': final['rec']}
-            connection.insert_one(row)
-            print (row)
+            print(final['acc'][-1])
+            row = {"id": str(output), 'v_mvp': output.v_mvp, 'v_mbp': output.v_mbp, 'v_oap': output.v_oap, 'v_interval':output.v_interval, "v_s": output.v_s, "accuracy": final['acc'], 'precision': final['pre'], 'recall': final['rec'], 'f1score': final['f1']}
+            # connection.insert_one(row)
+            # print (row)
             break
 
         next_car_index+=1
@@ -91,7 +99,3 @@ for output in named_product(v_s = [11000], v_mvp=[0.2], v_mbp=[0.5], v_oap=[0.2,
     result_values = defaultdict(lambda:[0,0,0]) #* precision, accuracy, recall
 
     final = defaultdict(list)
-
-
-
-
